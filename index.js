07d6dfef7fe3a12784a826b6f3fc9117acf9f7be
UmamiAppearance <mail@umamiappearance.eu>
2022-05-31 14:12:31 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// starting point from rollup-plugin-strip-code
+
+const createFilter = require('rollup-pluginutils').createFilter
+const MagicString = require('magic-string')
+
+const START_COMMENT = 'start_comment'
+const END_COMMENT = 'end_comment'
+
+function stripCode (options = {}) {
+  const { include, exclude, sourceMap, pattern, sourcemap } = options
+
+  const filter = createFilter(include, exclude)
+
+  return {
+    name: 'stripCode',
+
+    transform (source, id) {
+      if (!filter(id)) return
+
+      const startComment = options.start_comment || START_COMMENT
+      const endComment = options.end_comment || END_COMMENT
+      const defaultPattern = new RegExp(`([\\t ]*\\/\\* ?${startComment} ?\\*\\/)[\\s\\S]*?(\\/\\* ?${endComment} ?\\*\\/[\\t ]*\\n?)`, 'g')
+      let map
+      const code = source.replace(pattern || defaultPattern, '')
+
+      if (sourceMap !== false && sourcemap !== false) {
+        const magicString = new MagicString(code)
+        map = magicString.generateMap({ hires: true })
+      }
+
+      return { code, map }
+    }
+  }
+}
+
+module.exports = stripCode
---
+++ tests/task.test.js
@@ -0,0 +1,110 @@
+// TODO: replace all
+const stripCode = require('../index')
+const rollupPluginUtils = require('../node_modules/rollup-pluginutils')
+
+const mockedFilter = jest.fn((id) => true)
+
+jest.mock('../node_modules/rollup-pluginutils', () => ({
+  createFilter: jest.fn(() => mockedFilter)
+}))
+
+describe('stripCode.', () => {
+
+  test('should has proper name', () => {
+    expect(stripCode().name).toBe('stripCode')
+  })
+
+  test('should return proper object', () => {
+    expect(typeof stripCode()).toBe('object')
+  })
+
+  test('should mock createFilter', () => {
+    const filterFn = rollupPluginUtils.createFilter()
+    expect(typeof filterFn).toBe('function')
+    expect(filterFn()).toBe(true)
+  })
+
+  describe('default settings.', () => {
+
+    test('should remove code', () => {
+
+      const obj = stripCode()
+
+      const source = `whatever\n
+      /* start_comment */ \n
+      text to remove\n
+      /* end_comment */`
+
+      const result = obj.transform(source).code
+
+      expect(result.trim()).toBe('whatever')
+    })
+
+    test('should not remove anything', () => {
+      const obj = stripCode()
+
+      const source = `whatever\n
+      /* whatever_comment */ \n
+      text to remove\n
+      /* ends_comment */`
+
+      const result = obj.transform(source).code
+
+      expect(result.trim()).toBe(source)
+    })
+
+  })
+
+  describe('override settings.', () => {
+
+    test('should remove code with overrided options', () => {
+      const obj = stripCode({
+        start_comment: 'START.TESTS_ONLY',
+        end_comment: 'END.TESTS_ONLY'
+      })
+
+      const source = `whatever\n
+      /* START.TESTS_ONLY */ \n
+      text to remove\n
+      /* END.TESTS_ONLY */`
+
+      const result = obj.transform(source).code
+
+      expect(result.trim()).toBe('whatever')
+    })
+
+    test('should remove code with standart comments', () => {
+      const obj = stripCode({
+        start_comment: 'START.TESTS_ONLY',
+        end_comment: 'END.TESTS_ONLY'
+      })
+
+      const source = `whatever\n
+      /* start_comment */ \n
+      text to remove\n
+      /* end_comment */`
+
+      const result = obj.transform(source).code
+
+      expect(result.trim()).toBe(source)
+    })
+
+    test('should not remove anything', () => {
+      const obj = stripCode({
+        start_comment: 'START.TESTS_ONLY',
+        end_comment: 'END.TESTS_ONLY'
+      })
+
+      const source = `whatever\n
+      /* whatever_comment */ \n
+      text to remove\n
+      /* ends_comment */`
+
+      const result = obj.transform(source).code
+
+      expect(result.trim()).toBe(source)
+    })
+
+  })
+
+})
