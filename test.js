c7f48048c9b88facf96eae48c8fa49b82ef6f8f1
UmamiAppearance <mail@umamiappearance.eu>
2022-06-02 11:42:13 +0200

    progress

--- test.js
+++ test.js
@@ -1,36 +1,38 @@
 const imports = `import name from "module-name";
-import * as name from "module-name";
-import { member } from "module-name";
+import * as name from "module-name";import { member } from "module-name";
 import { member as alias } from "module-name";
 import { member1 , member2 } from "module-name";
 import { member1 , member2 as alias2 , [...] } from "module-name";
 import defaultMember, { member [ , [...] ] } from "module-name";
 import defaultMember, * as alias from "module-name";
 import defaultMember from "module-name";
-import "module-name";
-const x = import("./module-path");
+/* jdwjd
+oh boy */ import "module-name";
 
+// goodbye
 
 code 1
 code 2
-/* NO */
+/* NO */ // ciao
 code 3
 /*
 NO
 */ code4
 code 5
-/* NO */ code 6 /* NO */
+/* NO */ code 6 /* NO */ code +
 code 7
 code 8 /*
 NO */code 9
+/* sdjiw */const x = import("./module-path");
 
 `
+
+const MagicString = require("magic-string")
 
 
 const manager = {
     codeArray: imports.split("\n"),
-    comments: {},
-    importsLines: {}
+    imports: []
 }
 
 
@@ -43,11 +45,22 @@ const testFollowUpComment = (line, lIndex, start, end) => {
     }
 }
 
-const getMLC = (line, mlc) => {
+const removeStrings = line => line.replace(/(["'`])(?:(?=(\\?))\2.)*?\1/g, "");
+
+handleSLC = (line) => {
+    const cleanedLine = line.replace(/\/\/.*/, "");
+    const match = line !== cleanedLine;
+    return [cleanedLine, match];
+}
+
+// recursive multiline match
+const getMLC = (line, mlc, modified=false) => {
     let subLine = "";
+
     if (!mlc) {
         const match = line.match(/\/\*/);
         if (match) {
+            modified = true;
             [ subLine, mlc ] = getMLC(line.slice(match.index+2), true);
             line = line.slice(0, match.index);
         }
@@ -56,6 +69,7 @@ const getMLC = (line, mlc) => {
     else {
         const match = line.match(/\*\//);
         if (match) {
+            modified = true;
             [ subLine, mlc ] = getMLC(line.slice(match.index+2), false);
         }
         line = "";
@@ -63,119 +77,41 @@ const getMLC = (line, mlc) => {
 
     line += subLine;
 
-    return [line, mlc];
+    return [line, mlc, modified];
 }
 
 let mlc = false;
 manager.codeArray.forEach((line, i) => {
     
-    // remove all strings to prevent 
-    // false positives
-    // eg const foo = "http://.." 
-    // or console.log("import bar")
-    let cleanedLine = line.replace(/(["'`])(?:(?=(\\?))\2.)*?\1/g, "");
-    
+    // remove all strings 
+    let cleanedLine = removeStrings(line);
+
     // remove single line comments
-    cleanedLine = cleanedLine.replace(/\/\/.*/, "");
-
-
-    [ cleanedLine, mlc ] = getMLC(cleanedLine, mlc);
-    // look for the beginning of multi line comments
+    let hasSLC;
+    [ cleanedLine, hasSLC ] = handleSLC(cleanedLine);
     
-/*
-    // match single line comments
-    if (slcMatch) {
-        const contentBefore = cleanedLine.slice(0, slcMatch.index);
-
-        // if the comment is the start of the line,
-        // there is nothing to worry about
-        if (!contentBefore.trim()) {
-            manager.comments[i] = { ignore: true };
-        }
-
-        else {
-            
-            // if it is an actual comment with content before
-            // store the start position to allow the analysis
-            // of the code before the comment
-
-            manager.comments[i] = {
-                ignore: false,
-                start: 0,
-                end: slcMatch.index
-            }
-
-        }
-    }
+    // remove multi line comments
+    let hasMLC;
+    [ cleanedLine, mlc, hasMLC ] = getMLC(cleanedLine, mlc);
     
-    else if (!mlc) {*/
-        //const mlcStart = cleanedLine.match(/\/\*/);
-        /*
-        if (mlcStart) {
-            
-            // also test here if there is code before the
-            // comment to worry about
-            if (!line.slice(0, line.index).trim()) {
-                manager.comments[i] = { ignore: true };
-            }
+    //console.log("line", i+1, "hasSLC", hasSLC, "hasMLC", hasMLC)
+    //console.log(i+1, ":", cleanedLine);
 
-            // test for a false positive otherwise
-            else  {
-                
-                start = 0;
-                end = mlcStart.index;
-
-                manager.comments[i] = {
-                    ignore: false,
-                    start,
-                    end
-                };
+    const iStatements = cleanedLine.match(/(import)/g);
+    if (iStatements) {
+        iStatements.forEach((statement, li) => {
+            const imp = {
+                index: i,
+                li,
+                line: new MagicString(line),
+                hasMLC,
+                hasSLC
             }
-            
-            // test if the comment is actually across
-            // multiple lines, in other words test if*/
-            // it /* ends */ directly
-/*
-            const matchEnd = line.match(/\*\//);
-            mlc = !(matchEnd);
-
-            // stop processing if there is any other 
-            // comment following
-            if (matchEnd) {
-                testFollowUpComment(line, i, start, end);
-            }
-        }
+            manager.imports.push(imp);
+            console.log(imp.line.toString());
+        });
     }
-
-    else if (mlc) {
-        const mlcEnd = line.match(/\*\//);
-
-        if (mlcEnd) {
-            mlc = false;
-
-            let start = mlcEnd.index+2;
-            end = -1
-
-            // test if there is code after the comment to worry about
-            if (!line.slice(start).trim()) {
-                manager.comments[i] = { ignore: true };
-            }
-            
-            else {   
-                testFollowUpComment(line, i, start, end);         
-                manager.comments[i] = {
-                    ignore: false,
-                    start,
-                    end
-                };
-            }
-
-        }
-
-        else {
-            manager.comments[i] = { ignore: true };
-        }
-    }*/
-    console.log(cleanedLine);
 });
+
+console.log(manager.imports);
 
