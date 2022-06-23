1f604ec61e8febb6bf837aa12ab1eaf62bfbb91b
UmamiAppearance <mail@umamiappearance.eu>
2022-06-23 22:28:55 +0200

    refinements on diff

--- cjs/import-manager.cjs
+++ cjs/import-manager.cjs
@@ -1260,30 +1260,35 @@ const simpleHash = (input) => {
 };
 
 /**
- * Adds an angle bracket to each line.
- * @param {string} sign - '>' or '<'
+ * Adds an angle bracket to each line of a
+ * text section.
+ * @param {string} angle - '>' or '<'
  * @param {string} txt - The text section.
  * @returns {string} - The given text section with an angle bracket and a space in front of each line. 
  */
-const addSign = (sign, txt) => {
+const addAngles = (angle, txt) => {
     const txtArr = txt.split("\n");
     let lastChar = "";
     if (txt.at(-1) === "\n") {
         lastChar = "\n";
         txtArr.pop();
     }
-    let output = txtArr.map(line => sign+line).join("\n");
+    let output = txtArr.map(line => `${angle} ${line}`).join("\n");
     output += lastChar;
     return output;
 };
 
 /**
  * Prints an output in the mould of GNU diff when
- * called no parameters other than the files. But
- * with colors.
+ * called with no parameters other than the files.
+ * But more picturesque, thanks to red and green
+ * colors...
+ * Also possible is a "file" mode. This variant
+ * shows the whole file with added and removed
+ * lines.
  * @param {string} source - The original code.
  * @param {string} code - The modified code.
- * @param {*} [diffOption] - As passed by the user. If the value is 'all' also unchanged code is printed.  
+ * @param {string} [diffOption] - As passed by the user. If the value is 'file' also unchanged code is printed.  
  */
 const showDiff = (filename, source, code, diffOption) => {
     const fileMode = diffOption == "file";
@@ -1301,9 +1306,9 @@ const showDiff = (filename, source, code, diffOption) => {
         diff.forEach((part) => {
             let msg;
             if (part.added) {
-                msg = colorette.green(addSign("+", part.value));
+                msg = colorette.green(addAngles(">", part.value));
             } else if (part.removed) {
-                msg = colorette.red(addSign("-", part.value));
+                msg = colorette.red(addAngles("<", part.value));
             } else {
                 msg = part.value;
             }
@@ -1320,36 +1325,28 @@ const showDiff = (filename, source, code, diffOption) => {
         
         for (const part of diff.hunks) {
 
-            let add = false;
-            let del = false;
-            const content = part.lines;
-
+            // add
             if (part.oldLines === 0) {
-                add = true;
-            } else {
-                del = part.newLines === 0;
-            }
-
-
-            if (add) {
                 let info = `${part.oldStart}a${part.newStart}`;
                 if (part.newLines > 1) {
                     info += `,${part.newStart+part.newLines-1}`;
                 }
                 console.log(colorette.bold(info));
-                content.forEach(line => console.log(colorette.green(`> ${line.slice(1)}`)));
+                part.lines.forEach(line => console.log(colorette.green(`> ${line.slice(1)}`)));
             }
             
-            else if (del) {
+            // delete
+            else if (part.newLines === 0) {
                 let info = String(part.oldStart);
                 if (part.oldLines > 1) {
                     info += `,${part.oldStart+part.oldLines-1}`;
                 }
                 info += `d${part.newLines}`;
                 console.log(colorette.bold(info));
-                content.forEach(line => console.log(colorette.red(`< ${line.slice(1)}`)));
+                part.lines.forEach(line => console.log(colorette.red(`< ${line.slice(1)}`)));
             }
             
+            // change
             else {
                 let info = String(part.oldStart);
                 if (part.oldLines > 1) {
@@ -1362,28 +1359,18 @@ const showDiff = (filename, source, code, diffOption) => {
                 console.log(colorette.bold(info));
                 
                 let plus = false;
-                content.forEach((line, i) => {
+                part.lines.forEach((line, i) => {
                     if (plus) {
                         console.log(colorette.green(`> ${line.slice(1)}`));
                     } else {
                         console.log(colorette.red(`< ${line.slice(1)}`));
-                        if (content[i+1].at(0) === "+") {
+                        if (part.lines[i+1].at(0) === "+") {
                             console.log("---");
                             plus = true;
                         }
                     }
                 });
             }
-
-
-            /*
-            console.log({
-                add,
-                del,
-                change,
-                text: content
-            });
-            */
         }
     }
      
--- src/diff.js
+++ src/diff.js
@@ -3,30 +3,35 @@ import { blue, bold, red, green, gray } from "colorette";
 
 
 /**
- * Adds an angle bracket to each line.
- * @param {string} sign - '>' or '<'
+ * Adds an angle bracket to each line of a
+ * text section.
+ * @param {string} angle - '>' or '<'
  * @param {string} txt - The text section.
  * @returns {string} - The given text section with an angle bracket and a space in front of each line. 
  */
-const addSign = (sign, txt) => {
+const addAngles = (angle, txt) => {
     const txtArr = txt.split("\n");
     let lastChar = "";
     if (txt.at(-1) === "\n") {
         lastChar = "\n";
         txtArr.pop();
     }
-    let output = txtArr.map(line => sign+line).join("\n");
+    let output = txtArr.map(line => `${angle} ${line}`).join("\n");
     output += lastChar;
     return output;
 }
 
 /**
  * Prints an output in the mould of GNU diff when
- * called no parameters other than the files. But
- * with colors.
+ * called with no parameters other than the files.
+ * But more picturesque, thanks to red and green
+ * colors...
+ * Also possible is a "file" mode. This variant
+ * shows the whole file with added and removed
+ * lines.
  * @param {string} source - The original code.
  * @param {string} code - The modified code.
- * @param {*} [diffOption] - As passed by the user. If the value is 'all' also unchanged code is printed.  
+ * @param {string} [diffOption] - As passed by the user. If the value is 'file' also unchanged code is printed.  
  */
 const showDiff = (filename, source, code, diffOption) => {
     const fileMode = diffOption == "file"
@@ -44,9 +49,9 @@ const showDiff = (filename, source, code, diffOption) => {
         diff.forEach((part) => {
             let msg;
             if (part.added) {
-                msg = green(addSign("+", part.value));
+                msg = green(addAngles(">", part.value));
             } else if (part.removed) {
-                msg = red(addSign("-", part.value));
+                msg = red(addAngles("<", part.value));
             } else {
                 msg = part.value;
             }
@@ -63,38 +68,28 @@ const showDiff = (filename, source, code, diffOption) => {
         
         for (const part of diff.hunks) {
 
-            let add = false;
-            let del = false;
-            let change = false;
-            const content = part.lines;
-
+            // add
             if (part.oldLines === 0) {
-                add = true;
-            } else {
-                del = part.newLines === 0;
-                change = !del;
-            }
-
-
-            if (add) {
                 let info = `${part.oldStart}a${part.newStart}`;
                 if (part.newLines > 1) {
                     info += `,${part.newStart+part.newLines-1}`;
                 }
                 console.log(bold(info));
-                content.forEach(line => console.log(green(`> ${line.slice(1)}`)));
+                part.lines.forEach(line => console.log(green(`> ${line.slice(1)}`)));
             }
             
-            else if (del) {
+            // delete
+            else if (part.newLines === 0) {
                 let info = String(part.oldStart);
                 if (part.oldLines > 1) {
                     info += `,${part.oldStart+part.oldLines-1}`;
                 }
                 info += `d${part.newLines}`;
                 console.log(bold(info));
-                content.forEach(line => console.log(red(`< ${line.slice(1)}`)));
+                part.lines.forEach(line => console.log(red(`< ${line.slice(1)}`)));
             }
             
+            // change
             else {
                 let info = String(part.oldStart);
                 if (part.oldLines > 1) {
@@ -107,28 +102,18 @@ const showDiff = (filename, source, code, diffOption) => {
                 console.log(bold(info));
                 
                 let plus = false;
-                content.forEach((line, i) => {
+                part.lines.forEach((line, i) => {
                     if (plus) {
                         console.log(green(`> ${line.slice(1)}`));
                     } else {
                         console.log(red(`< ${line.slice(1)}`));
-                        if (content[i+1].at(0) === "+") {
+                        if (part.lines[i+1].at(0) === "+") {
                             console.log("---");
                             plus = true;
                         }
                     }
                 });
             }
-
-
-            /*
-            console.log({
-                add,
-                del,
-                change,
-                text: content
-            });
-            */
         }
     }
      
