9a036788b2ffaa7b5a58e78196039f52a646c732
UmamiAppearance <mail@umamiappearance.eu>
2022-07-25 14:05:53 +0200

    all tests done but one

--- tests/other.test.js
+++ tests/other.test.js
@@ -59,3 +59,89 @@ test("testing absence of match error if no file attribute passed", async (t) => 
     t.truthy(bundle);
 });
 
+
+test("debugging a file (default method)", async (t) => {
+    
+    const debug = await t.throwsAsync(() => {
+        return rollup({
+            input: "./tests/fixtures/hi.es6.js",
+            plugins: [
+                importManager({
+                    debug: null
+                })
+            ]
+        }); 
+    }, { instanceOf: DebuggingError });
+
+    const msg = debug.message;
+    t.true(msg.indexOf("3832119296") > -1);
+
+});
+
+
+test("debugging a file (verbose method)", async (t) => {
+    
+    const debug = await t.throwsAsync(() => {
+        return rollup({
+            input: "./tests/fixtures/hi.es6.js",
+            plugins: [
+                importManager({
+                    debug: "verbose"
+                })
+            ]
+        }); 
+    }, { instanceOf: DebuggingError });
+
+    const es6Imports = JSON.parse(debug.message).es6.units;
+    t.is(es6Imports.at(0).hash, "3832119296");
+    
+});
+
+
+test("testing include/exclude keyword", async (t) => {
+    
+
+    const plain = await t.throwsAsync(() => {
+        return rollup({
+            input: "./tests/fixtures/hi.dynamic.js",
+            plugins: [
+                importManager({
+                    debug: "verbose"
+                })
+            ]
+        }); 
+    }, { instanceOf: DebuggingError });
+
+    const include = await t.throwsAsync(() => {
+        return rollup({
+            input: "./tests/fixtures/hi.dynamic.js",
+            plugins: [
+                importManager({
+                    include: "**/hello.js",
+                    debug: "verbose"
+                })
+            ]
+        });
+    }, { instanceOf: DebuggingError });
+
+    
+    const exclude = await t.throwsAsync(() => {
+        return rollup({
+            input: "./tests/fixtures/hi.dynamic.js",
+            plugins: [
+                importManager({
+                    exclude: "**/hi.dynamic.js",
+                    debug: "verbose"
+                })
+            ]
+        });
+    }, { instanceOf: DebuggingError });
+
+    const plainDynImports = JSON.parse(plain.message).dynamic;
+    const includeDynImports = JSON.parse(include.message).dynamic;
+    const excludeDynImports = JSON.parse(exclude.message).dynamic;
+
+    t.is(plainDynImports.count, 1);
+    t.is(includeDynImports.count, 0);
+    t.is(excludeDynImports.count, 0);
+});
