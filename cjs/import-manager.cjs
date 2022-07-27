eac7446fbc41f30e13b59087b987403c688a4e37
UmamiAppearance <mail@umamiappearance.eu>
2022-07-27 22:07:40 +0200

    v0.1.3

--- cjs/import-manager.cjs
+++ cjs/import-manager.cjs
@@ -1296,7 +1296,7 @@ const showDiff = (filename, source, code, diffOption) => {
 /**
  * [rollup-plugin-import-manager]{@link https://github.com/UmamiAppearance/rollup-plugin-import-manager}
  *
- * @version 0.1.1
+ * @version 0.1.3
  * @author UmamiAppearance [mail@umamiappearance.eu]
  * @license MIT
  */
