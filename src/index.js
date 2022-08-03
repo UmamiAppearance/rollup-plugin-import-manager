96b1fb0d0794603e8860862e878582bbd4ce25ea
UmamiAppearance <mail@umamiappearance.eu>
2022-08-03 13:35:06 +0200

    v0.1.4

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
  * [rollup-plugin-import-manager]{@link https://github.com/UmamiAppearance/rollup-plugin-import-manager}
  *
- * @version 0.1.3
+ * @version 0.1.4
  * @author UmamiAppearance [mail@umamiappearance.eu]
  * @license MIT
  */
