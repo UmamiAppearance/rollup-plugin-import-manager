f736ca30d0fe5af207d2ddcdca7e0dd26016d6be
UmamiAppearance <mail@umamiappearance.eu>
2022-08-24 13:41:14 +0200

    v0.3.1

--- cjs/rollup-plugin-import-manager.cjs
+++ cjs/rollup-plugin-import-manager.cjs
@@ -1,3 +1,134 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var pluginutils = require('@rollup/pluginutils');
+var importManager$1 = require('import-manager');
+var diff = require('diff');
+var colorette = require('colorette');
+
+/**
+ * Adds an angle bracket to each line of a
+ * text section.
+ * @param {string} angle - '>' or '<'
+ * @param {string} txt - The text section.
+ * @returns {string} - The given text section with an angle bracket and a space in front of each line. 
+ */
+const addAngles = (angle, txt) => {
+    const txtArr = txt.split("\n");
+    let lastChar = "";
+    if (txt.at(-1) === "\n") {
+        lastChar = "\n";
+        txtArr.pop();
+    }
+    let output = txtArr.map(line => `${angle} ${line}`).join("\n");
+    output += lastChar;
+    return output;
+};
+
+
+/**
+ * Prints an output in the mould of GNU diff when
+ * called with no parameters other than the files.
+ * But more picturesque, thanks to red and green
+ * colors...
+ * Also possible is a "file" mode. This variant
+ * shows the whole file with added and removed
+ * lines.
+ * @param {string} source - The original code.
+ * @param {string} code - The modified code.
+ * @param {string} [diffOption] - As passed by the user. If the value is 'file' also unchanged code is printed.  
+ */
+const showDiff = (filename, source, code, diffOption) => {
+    const fileMode = diffOption == "file";
+    console.log(code);
+
+    console.log(colorette.bold(colorette.blue(
+        `(plugin ImportManager) diff for file '${filename}':`
+    )));
+    
+    console.log(colorette.gray("BEGIN >>>"));
+
+    if (fileMode) {
+        const diff$1 = diff.diffLines(source, code);
+
+        let message = "";
+        
+        diff$1.forEach((part) => {
+            let msg;
+            if (part.added) {
+                msg = colorette.green(addAngles(">", part.value));
+            } else if (part.removed) {
+                msg = colorette.red(addAngles("<", part.value));
+            } else {
+                msg = part.value;
+            }
+            message += msg;
+        });
+        
+        console.log(message);
+    
+    }
+        
+    else {
+        const diff$1 = diff.structuredPatch("", "", source, code, "", "", {
+            context: 0
+        });
+        
+        for (const part of diff$1.hunks) {
+
+            // add
+            if (part.oldLines === 0) {
+                let info = `${part.oldStart}a${part.newStart}`;
+                if (part.newLines > 1) {
+                    info += `,${part.newStart+part.newLines-1}`;
+                }
+                console.log(colorette.bold(info));
+                part.lines.forEach(line => console.log(colorette.green(`> ${line.slice(1)}`)));
+            }
+            
+            // delete
+            else if (part.newLines === 0) {
+                let info = String(part.oldStart);
+                if (part.oldLines > 1) {
+                    info += `,${part.oldStart+part.oldLines-1}`;
+                }
+                info += `d${part.newLines}`;
+                console.log(colorette.bold(info));
+                part.lines.forEach(line => console.log(colorette.red(`< ${line.slice(1)}`)));
+            }
+            
+            // change
+            else {
+                let info = String(part.oldStart);
+                if (part.oldLines > 1) {
+                    info += `,${part.oldStart+part.oldLines-1}`;
+                }
+                info += `c${part.newStart}`;
+                if (part.newLines > 1) {
+                    info += `,${part.newStart+part.newLines-1}`;
+                }
+                console.log(colorette.bold(info));
+                
+                let plus = false;
+                part.lines.forEach((line, i) => {
+                    if (plus) {
+                        console.log(colorette.green(`> ${line.slice(1)}`));
+                    } else {
+                        console.log(colorette.red(`< ${line.slice(1)}`));
+                        if (part.lines[i+1].at(0) === "+") {
+                            console.log("---");
+                            plus = true;
+                        }
+                    }
+                });
+            }
+        }
+    }
+     
+    console.log(colorette.gray("<<< END\n"));
+};
+
 /**
  * [rollup-plugin-import-manager]{@link https://github.com/UmamiAppearance/rollup-plugin-import-manager}
  *
@@ -5,11 +136,6 @@
  * @author UmamiAppearance [mail@umamiappearance.eu]
  * @license MIT
  */
-
-
-import { createFilter } from "@rollup/pluginutils";
-import { ImportManager } from "import-manager";
-import showDiff from "./diff.js";
 
 // test if input is an object
 const isObject = input => typeof input === "object" && !Array.isArray(input) && input !== null;
@@ -46,7 +172,7 @@ const showObjects = (v) => (/^(?:objects?|imports?|verbose)$/).test(String(v));
 // main
 const importManager = (options={}) => {
 
-    const filter = createFilter(options.include, options.exclude);
+    const filter = pluginutils.createFilter(options.include, options.exclude);
 
     // Initialize a new set to be passed to every 
     // ImportManager instance. It keeps track of
@@ -61,7 +187,7 @@ const importManager = (options={}) => {
 
             const warnings = typeof options.warnings === "undefined" ? true : bool(options.warnings);
 
-            const manager = new ImportManager(source, id, warnSpamProtection, warnings);       
+            const manager = new importManager$1.ImportManager(source, id, warnSpamProtection, warnings);       
 
             if (!("units" in options) || "debug" in options) {
                 if (showObjects(options.debug)) {
@@ -79,7 +205,7 @@ const importManager = (options={}) => {
                     let allowNull = true;
 
                     if ("file" in unitSection) {
-                        const isMatch = createFilter(unitSection.file);
+                        const isMatch = pluginutils.createFilter(unitSection.file);
 
                         if (!isMatch(id)) {
                             continue;
@@ -294,5 +420,6 @@ const importManager = (options={}) => {
         }
     };
 };
-  
-export { importManager };
+
+exports.importManager = importManager;
+//# sourceMappingURL=rollup-plugin-import-manager.cjs.map
